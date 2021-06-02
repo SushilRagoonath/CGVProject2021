53503348b1776c22ab710c465f13144a6ecc6b40
SushilRagoonath <Sushil29.r@gmail.com>
2021-06-02 23:37:01 +0200

    timer points win screen flag hit boxes

--- Level1/main.js
+++ Level1/main.js
@@ -1,5 +1,5 @@
 
-
+document.addEventListener('keydown',resetLevel)
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 85, window.innerWidth / window.innerHeight, 0.1, 1000 );
 clock = new THREE.Clock();
@@ -41,65 +41,99 @@ stellarBackground = new THREE.CubeTextureLoader()
 	'negz.png'
 ] );
 
+
 setTimeout(function(){ createRings() },200)
+scene.background = stellarBackground;
+scene.environment = stellarBackground;
 loadSpaceShip(function(){ //callback after loaded
 	xWingBox = new THREE.Box3();
 	xWingBox.setFromObject(xWing.scene);
-	console.log("xWing AABB",xWingBox)
-	player = new THREE.Group();
+	player = new Player();
 	player.add( camera );
 	player.add( xWing.scene);
 	scene.add( player );
+	document.addEventListener('keydown', player.changeView);
+
 	const helper = new THREE.Box3Helper( xWingBox, 0xffff00 );
 	scene.add(helper)
 	firstcontrols= new THREE.FirstPersonControls(player);
 	firstcontrols.lookSpeed = 0.05;
 	firstcontrols.movementSpeed = 10;
-
-	xWingShadow = new ShadowMesh( xWing.scene);
-	scene.add( xWingShadow );
-
-	camera.position.set( 0, 2,5*Math.sin(clock.getElapsedTime()));
-	// animate()// watch out for async. May need to be put in a setTimeout
-	
 })
+camera.position.set( 0, 1,-0.5);
+hp = 50;
+timeLeft = 10;
+
 createAtmosphericBoulders()
 createDodgeBoulders()
+setTimeout(function(){
+flag = createFlag(stellarBackground,checkeredTexture);
+flag.position.set(0,0,2800)
+flagBox = new THREE.Box3();
+flagBox.setFromObject(flag);
+var flagHelper = new THREE.Box3Helper(flagBox,0xffff00);
+scene.add(flag)
+scene.add(flagHelper)
+flag.scale.set(0.5,0.5,0.5)
 
-scene.background = stellarBackground;
-scene.environment = stellarBackground;
-
+},500)
 
 setupAudio()
 
 function animate() {
 	var delta = clock.getDelta();
-	camera.position.set( 0, 1,-0.5);
-	camera.position.set( 0, 2,5*Math.sin(clock.getElapsedTime()));
-	// firstcontrols.moveForward=true //keeps ship moving
-	firstcontrols.movementSpeed= 85
+	timeLeft -= delta;
+	// camera.position.set( 0, 1,-0.5);
+	// camera.position.set( 0, 2,5*Math.sin(clock.getElapsedTime()));
+	// if(timeLeft<57){
+	// 	firstcontrols.moveForward=true //keeps ship moving
+	// }
+	firstcontrols.movementSpeed= 75
 	firstcontrols.update(delta);
 	xWingBox.setFromObject(xWing.scene)
+	if(flagBox.intersectsBox(xWingBox)){
+		showGameWon()
+	}
 	let ringsToDelete=[]; //keeps track of collided rings
 	let ringBoxToDelete =-1;
+
 	for (let index = 0; index <ringBoxes.length; index++) {
 		const box = ringBoxes[index];
 		if(box.intersectsBox(xWingBox)){
 			console.log('ring collision')
 			ringsToDelete.push("ring"+String(index) ) 
-			ringsToDelete.push("helper"+String(index) ) 
+			// ringsToDelete.push("helper"+String(index) ) 
 			ringBoxToDelete = index	
+		}
+	}
+	for (var i = 0; i < boulderBoxes.length; i++) {
+		if(boulderBoxes[i].intersectsBox(xWingBox)){
+			hp-=1;
+			console.log('collision with rock',hp)
 		}
 	}
 	setTimeout(function(){ //setTimeout for delaying deletion
 		for (let index = 0; index < ringsToDelete.length; index++) {
+			if(scene.getObjectByName(ringsToDelete[index])!==undefined){
+				ringsRemoved +=1;
+				timeLeft +=2.2
+				console.log(ringsRemoved)
+			}
 			scene.remove(scene.getObjectByName(ringsToDelete[index]) )
 		}
+		
 
 	},100)
-	updateTimer()
+
+	updateUI()
+	if(hp<0 ||timeLeft <0){
+		console.warn("game over")
+		showGameOver()
+		restartLevel()
+	}
     renderer.render( scene, camera );
-	// composer.render()
 	requestAnimationFrame( animate );
 }
 setTimeout(animate,1000) // may need to be longer for more assets
+
+
---
+++ common/flag.js
@@ -0,0 +1,40 @@
+function createFlag(env,texture){
+                const flag = new THREE.Group();
+
+                var flag_geometry = new THREE.BoxGeometry( 50, 40, 50 );
+                var flag_material = new THREE.MeshStandardMaterial( { color: 0xff0000} );
+                const cube = new THREE.Mesh( flag_geometry, flag_material );
+                cube.name="flagBase"
+                cube.position.set(0,0,0)
+                flag.add( cube );
+                
+                flag_geometry = new THREE.CylinderGeometry( 5, 5, 300, 32 );
+                flag_material = new THREE.MeshPhysicalMaterial( { color: 0xffffff,metalness:0.5,roughness:0.2,envMap:env } );//0x00ff00
+                const pole = new THREE.Mesh( flag_geometry, flag_material );
+                pole.name = "flagPole"
+                pole.position.set(0,130,0)
+                flag.add(pole)
+
+                flag_geometry = new THREE.SphereGeometry( 10, 32, 32 );
+                flag_material = new THREE.MeshPhysicalMaterial( { color: 0xe0a10d,metalness:0.5,roughness:0.1,envMap:env} );
+                const sphere = new THREE.Mesh( flag_geometry, flag_material );
+                sphere.position.set(0,280,0)
+                sphere.name = "flagTop"
+                flag.add(sphere)
+
+                const verticesOfTriangle = [
+                    -1,1,0,    -1,-1,0,   1,0,0
+                ];
+
+                const indicesOfFaces = [
+                    0,1,2,
+                ];
+
+                flag_geometry = new THREE.PolyhedronGeometry( verticesOfTriangle, indicesOfFaces, 40, 0 );
+                flag_material = new THREE.MeshPhysicalMaterial( { color: 0xffffff,side: THREE.DoubleSide,map:texture} );
+                const triangle = new THREE.Mesh( flag_geometry, flag_material );
+                triangle.name="flagTriangle"
+                triangle.position.set(30,240,0)
+                flag.add(triangle)
+                return flag
+}
